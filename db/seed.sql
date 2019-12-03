8d2538c2cf7e792299e566e86fc51d8a1537a048
Jason Osao <josako89@gmail.com>
2019-12-03 01:13:07 -0800

    modified schema and seeds

--- db/seed.sql
+++ db/seed.sql
@@ -3,31 +3,31 @@ use employees;
 INSERT INTO department
     (name)
 VALUES
-    ('Sales'),
+    ('Accounting'),
     ('Engineering'),
     ('Finance'),
-    ('Legal');
+    ('Security');
 
 INSERT INTO role
     (title, salary, department_id)
 VALUES
-    ('Sales Lead', 100000, 1),
-    ('Salesperson', 80000, 1),
-    ('Lead Engineer', 150000, 2),
-    ('Software Engineer', 120000, 2),
-    ('Account Manager', 160000, 3),
-    ('Accountant', 125000, 3),
-    ('Legal Team Lead', 250000, 4),
-    ('Lawyer', 190000, 4);
+    ('CPA', 140000, 1),
+    ('Senior Accountant', 100000, 1),
+    ('Senior Engineer', 250000, 2),
+    ('Mechanical Engineer', 160000, 2),
+    ('Project Manager', 175000, 3),
+    ('CFA', 225000, 3),
+    ('Head of Security', 100000, 4),
+    ('Patrol', 40000, 4);
 
 INSERT INTO employee
     (first_name, last_name, role_id, manager_id)
 VALUES
-    ('John', 'Doe', 1, NULL),
-    ('Mike', 'Chan', 2, 1),
-    ('Ashley', 'Rodriguez', 3, NULL),
-    ('Kevin', 'Tupik', 4, 3),
-    ('Kunal', 'Singh', 5, NULL),
-    ('Malia', 'Brown', 6, 5),
-    ('Sarah', 'Lourd', 7, NULL),
-    ('Tom', 'Allen', 8, 7);
+    ('Ken', 'Nguyen', 1, NULL),
+    ('Mike', 'Chen', 2, 1),
+    ('Ariel', 'Hernandez', 3, NULL),
+    ('Kimberly', 'Patel', 4, 3),
+    ('Joshua', 'Redding', 5, NULL),
+    ('Maleia', 'Roman', 6, 5),
+    ('Bowzer', 'Bowzer', 7, NULL),
+    ('Tim', 'Hinske', 8, 7);
